c23ead9ecf4631ae036c2967637c9d2fd42bf304
tuacy <1007178106@qq.com>
2018-12-13 21:54:20 +0800

    cdk 完善实例

--- src/app/overlay/panel/overlay-panel.component.ts
+++ src/app/overlay/panel/overlay-panel.component.ts
@@ -2,8 +2,17 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-overlay-panel',
-  templateUrl: './overlay-panel.component.html',
-  styleUrls: ['./overlay-panel.component.less']
+  template: `
+    <p class="wu-overlay-pane">Overlay展示</p>
+  `,
+  styles: [`
+    .wu-overlay-pane {
+      margin: 0;
+      padding: 10px;
+      border: 1px solid black;
+      background-color: skyblue;
+    }
+  `]
 })
 export class OverlayPanelComponent implements OnInit {
 
