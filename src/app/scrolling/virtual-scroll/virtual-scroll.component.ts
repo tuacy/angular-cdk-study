02e06c9374fa59d4cc7e37e51c4278ed592bd29b
wuyx <1007178106@qq.com>
2018-12-15 16:12:38 +0800

    完善实例

--- src/app/scrolling/virtual-scroll/virtual-scroll.component.ts
+++ src/app/scrolling/virtual-scroll/virtual-scroll.component.ts
@@ -1,12 +1,22 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {CdkVirtualScrollViewport} from "@angular/cdk/scrolling";
 
 @Component({
     selector: 'app-virtual-scroll',
     templateUrl: './virtual-scroll.component.html',
     styleUrls: ['./virtual-scroll.component.less']
 })
-export class VirtualScrollComponent {
+export class VirtualScrollComponent implements AfterViewInit{
+
+    @ViewChild('scrollComponent')
+    private _scrollViewport: CdkVirtualScrollViewport;
 
     items = Array.from({length: 100000}).map((_, i) => `Item #${i}`);
 
+    ngAfterViewInit(): void {
+    }
+
+    onButtonClick() {
+        this._scrollViewport.scrollToIndex(10);
+    }
 }
